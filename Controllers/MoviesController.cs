b7e75a1c1bbedb2eb7f10966d9cb52a9b45d5890
samseppiol <j_hage@live.com>
2017-09-28 07:46:04 +0100

    Model, view, controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly_two.Models;
+
+namespace vidly_two.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Batman" };
+            return View(movie);
+        }
+    }
+}
